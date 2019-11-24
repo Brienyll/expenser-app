9ea158434ddf9094e872c33835cebd81ce1f04b3
Brienyll <Brienyll29@gmail.com>
2019-11-23 17:16:22 -0800

    added nested components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,8 +37,15 @@ class Options extends React.Component {
     return (
       <div>
         <p>Options Component</p>
+        <OptionComponents />
       </div>
     );
+  }
+}
+
+class OptionComponents extends React.Component {
+  render() {
+    return <p> Option Components </p>;
   }
 }
 
@@ -39,13 +59,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
