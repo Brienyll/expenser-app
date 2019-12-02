c5691e9fbf061d8cc588073bb1d26dc93690935d
Brienyll <Brienyll29@gmail.com>
2019-12-02 00:53:58 -0800

    added explicitly return object

--- src/app.js
+++ src/app.js
@@ -10,11 +10,7 @@ class IndecisionApp extends React.Component {
   }
 
   handleDeleteOptions() {
-    this.setState(() => {
-      return {
-        options: []
-      };
-    });
+    this.setState(() => ({ options: [] }));
   }
 
   handlePick() {
@@ -29,11 +25,9 @@ class IndecisionApp extends React.Component {
     } else if (this.state.options.indexOf(option) > -1) {
       return 'Option already exists';
     } else {
-      this.setState(prevState => {
-        return {
-          options: prevState.options.concat([option])
-        };
-      });
+      this.setState(prevState => ({
+        options: prevState.options.concat([option])
+      }));
     }
   }
   render() {
@@ -162,9 +156,7 @@ class AddOption extends React.Component {
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
 
-    this.setState(() => {
-      return { error };
-    });
+    this.setState(() => ({ error }));
   }
   render() {
     return (
