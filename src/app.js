e1a43d966055402e5246ba5a403c59251391ef53
Brienyll <Brienyll29@gmail.com>
2019-12-10 18:21:01 -0800

    removed constructor - to class properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class NewSyntax {
+  name = 'Gen';
+  getGreeting = () => {
+    return `Hi. My name is ${this.name}`;
+  };
+}
+
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
