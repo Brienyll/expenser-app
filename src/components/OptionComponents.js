2faa8d407f2e7a5e6a10c31ab8158dda39955843
Brienyll <Brienyll29@gmail.com>
2019-12-11 19:28:18 -0800

    refactored stateless components

--- src/components/OptionComponents.js
+++ src/components/OptionComponents.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const OptionComponents = props => {
-  return (
-    <div>
-      <p>{props.optionText}</p>
-      <button
-        onClick={e => {
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        remove
-      </button>
-    </div>
-  );
-};
+const OptionComponents = props => (
+  <div>
+    <p>{props.optionText}</p>
+    <button
+      onClick={e => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      remove
+    </button>
+  </div>
+);
 
 export default OptionComponents;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import OptionComponents from './OptionComponents';
 
-const Options = props => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && (
-        <p> Please add an option to get started!</p>
-      )}
-      {props.options.map(option => (
-        <OptionComponents
-          key={option}
-          optionText={option}
-          handleDeleteOption={props.handleDeleteOption}
-        />
-      ))}
-    </div>
-  );
-};
+const Options = props => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p> Please add an option to get started!</p>}
+    {props.options.map(option => (
+      <OptionComponents
+        key={option}
+        optionText={option}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
+  </div>
+);
 
 export default Options;
