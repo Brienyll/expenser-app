f67d31759af35f6223a381e22cbf40e32e77eb77
Brienyll <Brienyll29@gmail.com>
2019-12-24 01:20:50 -0800

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 // const person = {
 //   name: 'Brix',
 //   age: 35,
@@ -17,14 +21,29 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const Book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: ' Penguin'
-  }
-};
+// const Book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: ' Penguin'
+//   }
+// };
 
-const { name: publisherName = 'self-published' } = Book.publisher;
+// const { name: publisherName = 'self-published' } = Book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+// const address = ['1299 Terra', 'Los Angeles', 'California', '90210'];
+
+// const [, city, state] = address;
+
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [coffee, , medium, large] = item;
+
+console.log(`A medium ${coffee} costs ${medium}`);
