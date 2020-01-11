d5d139ca7dcbaf0dc3bedb611d88e12ad0b8ca41
Brienyll <Brienyll29@gmail.com>
2020-01-11 13:22:02 -0800

    added test for action generators

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,49 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  });
+});
+
+test('test edit expense', () => {
+  const action = editExpense('234bcd', { note: 'New note' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '234bcd',
+    updates: { note: 'New note' }
+  });
+});
+
+test('setup add expense action object with values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109100,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('setup add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expect.any(String),
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
+    }
+  });
+});
