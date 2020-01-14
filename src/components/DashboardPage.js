71c0459c02ffdbd072171ed20b803aeaafc50fde
Brienyll <Brienyll29@gmail.com>
2020-01-14 07:09:34 -0800

    snapshot tested dynamic components

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
-const DashboardPage = () => (
+export const DashboardPage = () => (
   <div>
     <ExpenseListFilters />
     <ExpenseList />
